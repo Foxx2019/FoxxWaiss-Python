a2d5fdb1878730a98d7757e28debd7fa6d56e355
grace123456 <19liuzhuoran@bprep.org>
2018-09-13 09:18:08 -0700

    unit 2c lab

---
+++ Unit2CLab/Unit2CLab.py
@@ -0,0 +1,31 @@
+numList = [11,12,13,14,15,16,17,18,19,20]
+print(numList)
+print(len(numList))
+print(numList[0])
+print(numList[9])
+SubList = numList[0:5]
+print(SubList)
+SubList.insert(3 , 2)
+print(SubList)
+SubList.append(23)
+print(SubList)
+myClasses = ['Math','Science','Astronomy','English','Ceramics','Python','Painting']
+print(myClasses)
+myClasses.remove('English')
+print(myClasses)
+favClass = myClasses.pop(4)
+print('my favorite class is ' + favClass)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
