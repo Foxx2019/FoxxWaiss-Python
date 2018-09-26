064cd0482c277e4b2369ec8d399b125d429ae3d5
Foxx2019 <42872503+Foxx2019@users.noreply.github.com>
2018-09-25 21:18:45 -0700

    Update Lab321.py

--- Lab321/Lab321.py
+++ Lab321/Lab321.py
@@ -1,8 +1,9 @@
 def main():
+    myList1 = [93.4, 86.8, 98.2, 89.9, 79.4, 94.6]
     gradeIn = input("what year are you in? - ")
     grade1 = int(gradeIn)
     print("you are " + grade(grade1))
-
+    print("your cureent GPA is - " + str(gpaFind(myList1))
 
 def grade(gradeIn):
     if gradeIn == 12:
@@ -15,11 +16,11 @@ def grade(gradeIn):
         return("a freshman")
     else:
         return("not in highschool you silly goose!")
-
-
-myList1 = [93.4, 86.8, 98.2, 89.9, 79.4, 94.6]
-print(len(myList1))
-
+          
+def gpaFind(myList1)
+    myList1 = [93.4, 86.8, 98.2, 89.9, 79.4, 94.6]
+    gpa = sum(myList1) / float(len(myList1))
+    return(gpa)
 
 
 
