f23e51ea8707d1987f405fcbcdcd63e0746fb6f9
grace123456 <19liuzhuoran@bprep.org>
2018-10-15 08:48:26 -0700

    yeet

--- Unit4CLab/Unit 4C Lab.py
+++ Unit4CLab/Unit 4C Lab.py
@@ -1,7 +1,30 @@
 def draw7():
-    for i in range (0,7):
-        for x in range (0,7):
-            print('*', end = '')
-    print()
+    for i in range(0,7):
+        starLoops()
+
+
+def starLoops():
+    for i in range(0,7):
+        starString = ''
+        for i in range(0,7):
+            starString = (starString + '*')
+    print(starString)
 
 draw7()
+
+input('pause')
+
+
+def starsAndStripes():
+    for i in range(0,3):
+        string1 = ''
+        string2 = ''
+        for i in range (0,7):
+            string1 = (string1 + '*')
+            string2 = (string2 + '-')
+        print(string1)
+        print(string2)
+
+starsAndStripes()
+
+#def incTriangle():
