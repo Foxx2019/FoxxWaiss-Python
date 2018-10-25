e67ed2b39772d2c4e80e6f89f1e88a882a6eeec0
grace123456 <19liuzhuoran@bprep.org>
2018-10-25 08:50:31 -0700

    yeooot

--- Unit-4D-Lab/Unit 4D Lab.py
+++ Unit-4D-Lab/Unit 4D Lab.py
@@ -1,17 +1,35 @@
 def main():
-    list1 = ['tooth paste', 'q-tips', 'milk']
-    list2 = ['milk', 'candy', 'apples']
-    list3 = ['paper', 'pencils', 'q-tips']
-    shoppingCart = [list1, list2, list3]
-    print(allInOne(cart))
+    shoppingCart = [['toothpaste', "q-tips", "milk"],["milk", "candy", "apples"],["paper", "pencils", "q-tips"]]
+    print(shoppingCart)
+    input('pause')
+    print(allInOne(shoppingCart))
+    input('pause')
+    print("Q-tips appear", countQtips(shoppingCart), "times")
 
 
 def allInOne(cart):
-    newList = []
+    newList1 = []
+    for list in cart:
+#goes into the 3 lists in shoppingCart
+        for item in list:
+#goes into the contents of each list
+            if item not in newList1:
+                newList1.append(item)
+#this allows only one of each item to be in newList1
+    return newList1
+
+
+def countQtips(cart):
+    newList2 = []
+    count = 0
     for list in cart:
         for item in list:
-            if item not in newList:
-                newList.append(item)
-    return newList
+            if item not in cart:
+                newList2.append(item)
+    for i in range(len(newList2)):
+        if newList2[i] == 'q-tips':
+            count += 1
+    return count
+
 
 main()
