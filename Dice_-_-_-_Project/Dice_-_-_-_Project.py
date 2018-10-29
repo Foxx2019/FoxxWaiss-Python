adc162f4dc722039320b1ac91b12b54957503915
grace123456 <19liuzhuoran@bprep.org>
2018-10-29 08:58:43 -0700

    beet the yeet

---
+++ Dice_-_-_-_Project/Dice_-_-_-_Project.py
@@ -0,0 +1,52 @@
+import random
+
+
+
+def randNum():
+    x = random.randint(1,6)
+    print(x)
+
+
+
+def diceOne():
+    print(' ------- ')
+    print('|       |')
+    print('|   *   |')
+    print('|       |')
+    print(' ------- ')
+
+def diceTwo():
+    print(' ------- ')
+    print('|       |')
+    print('|  * *  |')
+    print('|       |')
+    print(' ------- ')
+
+def diceThree():
+    print(' ------- ')
+    print('| *     |')
+    print('|   *   |')
+    print('|     * |')
+    print(' ------- ')
+
+def diceFour():
+    print(' ------- ')
+    print('| *   * |')
+    print('|       |')
+    print('| *   * |')
+    print(' ------- ')
+
+def diceFive():
+    print(' ------- ')
+    print('| *   * |')
+    print('|   *   |')
+    print('| *   * |')
+    print(' ------- ')
+
+def diceSix():
+    print(' ------- ')
+    print('| *   * |')
+    print('| *   * |')
+    print('| *   * |')
+    print(' ------- ')
+
